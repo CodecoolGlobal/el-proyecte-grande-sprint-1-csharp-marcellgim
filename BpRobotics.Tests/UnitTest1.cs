ba8a26caadfd7e12b442d4a723aaf80031294512
skris92 <szurcsik92@gmail.com>
2022-05-24 14:38:53 +0200

    Api testing

--- BpRobotics.Tests/UnitTest1.cs
+++ BpRobotics.Tests/UnitTest1.cs
@@ -1,16 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace BpRobotics.Tests
 {
     public class Tests
     {
+        private HttpClient _httpClient;
         [SetUp]
         public void Setup()
         {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            _httpClient = webAppFactory.CreateDefaultClient();
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            var response = await _httpClient.GetAsync("WeatherForecast/hello-world");
+            string result = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("Hello World!", result);
         }
     }
 }
