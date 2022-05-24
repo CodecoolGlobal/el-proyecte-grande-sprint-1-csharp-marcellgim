a301b9b0f95e8c677b960c68070a4e593ec8a756
marcellgim <marcell.gimesi@gmail.com>
2022-05-24 11:36:44 +0200

    Starter project files

---
+++ BpRobotics/BpRobotics.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BpRobotics.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
