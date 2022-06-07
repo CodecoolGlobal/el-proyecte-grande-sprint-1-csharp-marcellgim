d807bca1cc14d2bbb2f30613a9b4083a9979efda
marcellgim <marcell.gimesi@gmail.com>
2022-06-07 15:17:12 +0200

    Create UserModel and UserService files
    Very early version

---
+++ BpRobotics.Core/Model/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using BpRobotics.Data.Model;
+
+namespace BpRobotics.Core.Model
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
