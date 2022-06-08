fda90af0e1a842ee11b72022a1a264e2fc8acc3d
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-06-08 15:17:13 +0200

    Creating Products frontend

--- BpRobotics/Client/bprobotics-client/src/components/Products.js
+++ BpRobotics/Client/bprobotics-client/src/components/Products.js
@@ -1,5 +1,43 @@
+import { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import { useNavigate } from "react-router-dom";
 function Products() {
-    return ( <div>Under construction</div> );
+    let url = "https://localhost:7166/api/products";
+    const [productData, setProductData] = useState([]);
+    let navigate = useNavigate(); 
+  function routeChange(productId){ 
+    let path = productId.toString(); 
+    navigate(path);
+  }
+    const fetchProductData = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+      }
+    
+      useEffect(() => {
+        const getProductData = async (url) => {
+          const productDataFromApi = await fetchProductData(url);
+          setProductData(productDataFromApi);
+        }
+        getProductData(url);
+      }, [url])
+    return ( <MaterialTable
+        title="Products"
+        columns={[
+            { title: '',
+            field: 'img',
+            render: item => <img onClick={() => routeChange(item.id)} src={item.img} alt="" height="100" width="100" />,
+            filtering: false
+        },
+          { title: 'Name', field: 'name' },
+          { title: 'Description', field: 'shortDescription' },
+        ]}
+        data={productData}        
+        options={{
+          filtering: true
+        }}
+      /> );
 }
 
 export default Products;
