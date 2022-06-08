c15d8d03f0a119068c4c3d986a5528449b74f3e2
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-06-08 15:16:23 +0200

    Adding IMG to Product model

--- BpRobotics.Data/Entity/Product.cs
+++ BpRobotics.Data/Entity/Product.cs
@@ -4,6 +4,8 @@ public class Product
 {
     public int Id { get; set; }
     public string Name { get; set; }
+
+    public string Img { get; set; }
     public TimeSpan ServiceInterval { get; set; }
     public int Warranty { get; set; }
     public string ShortDescription { get; set; }
