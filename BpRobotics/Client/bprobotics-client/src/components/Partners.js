2094fc0bc8067e22600fce1613f6730a652d0e4a
vmarcell96 <vajdam96@gmail.com>
2022-06-08 17:39:08 +0200

    it works with material table

--- BpRobotics/Client/bprobotics-client/src/components/Partners.js
+++ BpRobotics/Client/bprobotics-client/src/components/Partners.js
@@ -4,32 +4,34 @@ import MaterialTable from 'material-table';
 
 
 function Partners() {
-    const [partnerData, setData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const response = await fetch(`${process.env.REACT_APP_URL}partners`);
-          const newData = await response.json();
-          setData(newData);
-        };
-    
-        fetchData();
-      }, []);
+    const [partnerData, setData] = useState([]);
 
 
+  useEffect(()=> {
+    fetch(`${process.env.REACT_APP_URL}partners`)
+    .then(response => response.json())
+    .then(data=>{
+    console.log(data);
+    setData(data);
+    }).catch( err=> {
+    console.log("Error Reading data " + err);
+  })
+  },[])
 
-    return ( 
-        <div style={{ maxWidth: '100%' }}>
-        <MaterialTable
-          columns={[
-            { title: 'Company name', field: 'CompanyName' },
-            { title: 'Mobile number', field: 'PhoneNumber' },
-          ]}
-          data={partnerData}
-          title="Partners"
-        />
-      </div>
-    );
+
+  return (
+      <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        columns={[
+          { title: 'Company name', field: 'companyName' },
+          { title: 'Mobile number', field: 'phoneNumber' },
+        ]}
+        data={partnerData}
+        title="Partners"
+      />
+    </div>
+  );
 }
 
 export default Partners;
