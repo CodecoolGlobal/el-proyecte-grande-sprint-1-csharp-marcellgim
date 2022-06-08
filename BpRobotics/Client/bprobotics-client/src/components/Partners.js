53b269ddc9ce0d4bf285b754b19523dd8b20858b
vmarcell96 <vajdam96@gmail.com>
2022-06-08 23:36:18 +0200

    Added form and post fetch function on submit

--- BpRobotics/Client/bprobotics-client/src/components/Partners.js
+++ BpRobotics/Client/bprobotics-client/src/components/Partners.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
 import MaterialTable from 'material-table';
+import '../App.css';
 
 
 
 function Partners() {
 
     const [partnerData, setData] = useState([]);
-
+    const [companyName, setCompanyName] = useState('hello');
+    const [phoneNumber, setPhoneNumber] = useState('+36');
+    const [isPending, setIsPending] = useState(false);
 
   useEffect(()=> {
-    fetch(`${process.env.REACT_APP_URL}partners`)
+    fetch(`${process.env.REACT_APP_URL}/partners`)
     .then(response => response.json())
     .then(data=>{
     console.log(data);
@@ -19,8 +22,45 @@ function Partners() {
   })
   },[])
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPartner = { companyName, phoneNumber };
+    
+    setIsPending(true);
+
+    fetch(`${process.env.REACT_APP_URL}/partners`, {
+      method: 'POST',
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(newPartner)
+    }).then(() => {
+      console.log('New partner added');
+      setIsPending(false);
+    })
+  }
 
   return (
+      <>
+      <div className="create">
+          <h2>Add new Partner</h2>
+          <form onSubmit={handleSubmit}>
+              <label>Company name:</label>
+              <input 
+                type="text" 
+                required 
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
+              />
+              <label>Phone number:</label>
+              <input 
+                type="text" 
+                required
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)} />
+              { !isPending && <button>Add partner</button> }
+              { isPending && <button>Adding partner...</button> }
+          </form>
+          
+      </div>
       <div style={{ maxWidth: '100%' }}>
       <MaterialTable
         columns={[
@@ -31,6 +71,7 @@ function Partners() {
         title="Partners"
       />
     </div>
+    </>
   );
 }
 
