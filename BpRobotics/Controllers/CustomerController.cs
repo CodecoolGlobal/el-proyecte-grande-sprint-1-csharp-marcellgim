d243854f9ba42f7570935e4604d175ef2245a543
Balázs Begov <bbegov@googlemail.com>
2022-06-08 13:42:08 +0200

    Controller created for customers with get routes

---
+++ BpRobotics/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using BpRobotics.Data.Model;
+using BpRobotics.Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BpRobotics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IRepository<Customer> _customerRepository;
+
+        public CustomerController(IRepository<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet("customers")]
+        public List<Customer> GetAllCustomers()
+        {
+            return _customerRepository.GetAll();
+        }
+
+        [HttpGet("customers/{id}")]
+        public Customer GetCustomerById([FromRoute] int id)
+        {
+            return _customerRepository.Get(id);
+        }
+    }
+}
