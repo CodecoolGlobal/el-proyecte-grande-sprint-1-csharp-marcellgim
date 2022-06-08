c58d9bcb579ce768aebcd275c6e3ac717b1cb3d7
Balázs Begov <bbegov@googlemail.com>
2022-06-08 13:25:29 +0200

    Id property added

--- BpRobotics.Data/Entity/Customer.cs
+++ BpRobotics.Data/Entity/Customer.cs
@@ -2,6 +2,7 @@
 
 public class Customer
 {
+    public int Id { get; set; }
     public string CompanyName { get; set; } = String.Empty;
     public int VatNumber { get; set; }
     public Location BillingAddress { get; set; }
