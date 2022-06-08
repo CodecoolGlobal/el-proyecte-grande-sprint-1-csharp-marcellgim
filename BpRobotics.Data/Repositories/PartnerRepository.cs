3db9cf81f6014c56a93911f0e96f382e08faf2a3
vmarcell96 <vajdam96@gmail.com>
2022-06-08 13:56:53 +0200

    Partnerrepository added

---
+++ BpRobotics.Data/Repositories/PartnerRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BpRobotics.Data.Model;
+
+namespace BpRobotics.Data.Repositories
+{
+    public class PartnerRepository : IRepository<Partner>
+    {
+        private readonly IBpRoboticsDataStorage _storage;
+
+        public PartnerRepository(IBpRoboticsDataStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public void Add(Partner entity)
+        {
+            _storage.Partners.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            _storage.Partners.Remove(Get(id));
+        }
+
+        public Partner Get(int id)
+        {
+            return _storage.Partners.Where(x => x.Id == id).First();
+        }
+
+        public List<Partner> GetAll()
+        {
+            return _storage.Partners.ToList();
+        }
+
+        public void Update(int id, Partner entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
