458b8c8c7ba198dd22748a6db570cb37eaba3628
marcellgim <marcell.gimesi@gmail.com>
2022-06-08 14:11:52 +0200

    Display all users in table
    Resolves #31

--- BpRobotics/Client/bprobotics-client/src/components/Users.js
+++ BpRobotics/Client/bprobotics-client/src/components/Users.js
@@ -1,5 +1,48 @@
+import { useState, useEffect } from 'react';
+import MaterialTable from "material-table";
+
 function Users() {
-    return ( <div>Under construction</div> );
+    const [userList, setUserList] = useState();
+
+    const columns = [
+        {title: "Username", field: "userName"},
+        {title: "First Name", field: "firstName"},
+        {title: "Last Name", field: "lastName"},
+        {
+            title: "Role",
+            field: "role",
+            render: rowData => {
+                switch (rowData.role) {
+                    case 0:
+                        return "Admin"
+                    case 1:
+                        return "Partner"
+                    case 2:
+                        return "Customer"
+                    default:
+                        return "Undefined"
+                }
+            }
+        }
+    ]
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await fetch(`${process.env.REACT_APP_HOST_URL}/users`);
+            const data = await response.json();
+            setUserList(data);
+        }
+        loadUsers();
+    }, [])
+
+    return (
+    <div>
+        <MaterialTable
+            title="List of users"
+            data={userList} columns={columns}
+        />
+    </div>
+    );
 }
 
 export default Users;
