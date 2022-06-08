45f47d52aca52056ed7f44475bede3cf92b3fc80
vmarcell96 <vajdam96@gmail.com>
2022-06-08 13:58:13 +0200

    partner.js has fetch method

--- BpRobotics/Client/bprobotics-client/src/components/Partners.js
+++ BpRobotics/Client/bprobotics-client/src/components/Partners.js
@@ -1,5 +1,39 @@
+import { useState, useEffect } from "react";
+
+function getPartners() {
+
+}
+
+
+
 function Partners() {
-    return ( <div>Under construction</div> );
+    const [partnerData, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_URL}partners`, {
+            mode: 'cors',
+            headers: {
+              'Access-Control-Allow-Origin':'*'
+            }
+          })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(
+                `This is an HTTP error: The status is ${response.status}`
+              );
+            }
+            return response.json();
+          })
+          .then((actualData) => console.log(actualData))
+          .catch((err) => {
+            console.log(err.message);
+          });
+      }, []);
+
+    return ( <div>asd{process.env.REACT_APP_URL}</div> );
 }
 
 export default Partners;
