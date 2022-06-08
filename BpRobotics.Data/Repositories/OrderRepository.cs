f35ac7e59b2a424bb56154f3a040c9193635423f
skris92 <szurcsik92@gmail.com>
2022-06-08 13:23:46 +0200

    OrderRepository with CRUD methods.

---
+++ BpRobotics.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using BpRobotics.Data.Model;
+
+namespace BpRobotics.Data.Repositories;
+
+public class OrderRepository : IRepository<Order>
+{
+    private readonly IBpRoboticsDataStorage _storage;
+
+    public OrderRepository(IBpRoboticsDataStorage storage)
+    {
+        _storage = storage;
+    }
+
+    public List<Order> GetAll() => _storage.Orders.ToList();
+
+    public Order Get(int id) => _storage.Orders.First(user => user.Id == id);
+
+    public void Delete(int id)
+    {
+        _storage.Orders.Remove(Get(id));
+    }
+
+    public void Add(Order entity)
+    {
+        _storage.Orders.Add(entity);
+    }
+
+    public void Update(int id, Order entity)
+    {
+        // TODO override properties
+        var userToUpdate = Get(id);
+    }
+}
