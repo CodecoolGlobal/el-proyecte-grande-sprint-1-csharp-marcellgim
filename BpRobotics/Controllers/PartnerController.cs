dfff8e77ece334af79c1d435bc3a91f23a29671b
vmarcell96 <vajdam96@gmail.com>
2022-06-08 13:57:18 +0200

    partnerController added with /partners node

---
+++ BpRobotics/Controllers/PartnerController.cs
@@ -0,0 +1,25 @@
+﻿using BpRobotics.Data.Model;
+using BpRobotics.Data.Repositories;
+using BpRobotics.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BpRobotics.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class PartnerController : Controller
+    {
+        private readonly IRepository<Partner> _partnerRepository;
+
+        public PartnerController(IRepository<Partner> partnerRepository)
+        {
+            _partnerRepository = partnerRepository;
+        }
+
+        [Route("partners")]
+        public List<Partner> ListPartners()
+        {
+            return _partnerRepository.GetAll();
+        }
+    }
+}
