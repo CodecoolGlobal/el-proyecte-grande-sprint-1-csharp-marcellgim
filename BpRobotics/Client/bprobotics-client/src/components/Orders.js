e21cc0ad43569e4be15a8e6a566e8b1d955c57fd
skris92 <szurcsik92@gmail.com>
2022-06-09 11:14:43 +0200

    Orders list page

--- BpRobotics/Client/bprobotics-client/src/components/Orders.js
+++ BpRobotics/Client/bprobotics-client/src/components/Orders.js
@@ -1,5 +1,37 @@
+import { useState, useEffect } from "react";
+import MaterialTable from "material-table";
+import { render } from "@testing-library/react";
+
 function Orders() {
-    return ( <div>Under construction</div> );
+    const url = `${process.env.REACT_APP_URL}/api/orders`;
+    const [fetchData, setData] = useState([]);
+
+    useEffect(()=> {
+        fetch(`${url}`)
+    .then(response => response.json())
+    .then(data=>{
+      console.log(data);
+      setData(data);
+    }).catch( err=> {
+      console.log("Error Reading data " + err);
+    })
+    },[])
+
+    return ( 
+        <div style={{ maxWidth: '100%' }}>
+            <MaterialTable
+            columns={[
+                { title: 'Date', field: 'date' },
+                { title: 'Company Name', field: 'customer.companyName' },
+                { title: 'Address', render: (rowData) => rowData.address.country+","+rowData.address.city+","+rowData.address.address+","+rowData.address.zip },
+                { title: 'Devices', render: (rowData) => <div>{(rowData.devices.map(device =><><div>{device.product.name},</div><br/></>))}</div>}
+            ]}
+            data={fetchData}
+            title="Orders"
+
+            />
+        </div>
+     );
 }
 
 export default Orders;
