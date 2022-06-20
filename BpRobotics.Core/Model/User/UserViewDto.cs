f4592ceed354990eb4f1bb829dbfe9e6a21b8048
marcellgim <marcell.gimesi@gmail.com>
2022-06-20 20:38:41 +0200

    Add user view DTO
    Closes #79

---
+++ BpRobotics.Core/Model/User/UserViewDto.cs
@@ -0,0 +1,11 @@
+﻿using BpRobotics.Data.Entity;
+
+namespace BpRobotics.Core.Model.User;
+
+public class UserViewDto
+{
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+}
