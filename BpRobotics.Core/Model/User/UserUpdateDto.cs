19ba71eed81881ff2758208822d729f5b652755f
marcellgim <marcell.gimesi@gmail.com>
2022-06-20 20:34:47 +0200

    Add user update DTO

---
+++ BpRobotics.Core/Model/User/UserUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BpRobotics.Core.Model.User;
+
+public class UserUpdateDto
+{
+    [Required]
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+}
