22e5f15ae2fedc4450e94099f9bf01bf4f7f35fd
marcellgim <marcell.gimesi@gmail.com>
2022-06-21 13:18:48 +0200

    Add id field and annotations

--- BpRobotics.Core/Model/User/UserUpdateDto.cs
+++ BpRobotics.Core/Model/User/UserUpdateDto.cs
@@ -6,7 +6,10 @@ public class UserUpdateDto
 {
     [Required]
     public int Id { get; set; }
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [Required]
     public string Password { get; set; }
 }
