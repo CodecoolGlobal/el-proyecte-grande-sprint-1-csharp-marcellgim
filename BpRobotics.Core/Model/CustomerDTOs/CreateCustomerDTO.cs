d1341fa720db4d48b26a558887ae21e450a1beff
Balázs Begov <bbegov@googlemail.com>
2022-06-21 13:15:16 +0200

    Customer DTOs created

---
+++ BpRobotics.Core/Model/CustomerDTOs/CreateCustomerDTO.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BpRobotics.Core.Model.CustomerDTOs
+{
+    public class CreateCustomerDTO
+    {
+        private const int MinZipValue = 1000;
+        private const int MaxZipValue = 9999;
+        private const int MaxStringLength = 100;
+
+        [Required]
+        [StringLength(maximumLength: MaxStringLength)]
+        public string CompanyName { get; set; } = string.Empty;
+        [Required]
+        public int VatNumber { get; set; }
+        [Required]
+        [Range(minimum: MinZipValue, maximum: MaxZipValue)]
+        public int BillingZIP { get; set; }
+        [Required]
+        [StringLength(maximumLength: MaxStringLength)]
+        public string BillingCountry { get; set; }
+        [Required]
+        [StringLength(maximumLength: MaxStringLength)]
+        public string BillingCity { get; set; }
+        [Required]
+        [StringLength(maximumLength: MaxStringLength)]
+        public string BillingAddress { get; set; }
+        [Required]
+        [Range(minimum: MinZipValue, maximum: MaxZipValue)]
+        public int ShippingZIP { get; set; }
+        [Required]
+        [StringLength(maximumLength: MaxStringLength)]
+        public string ShippingCountry { get; set; }
+        [Required]
+        [StringLength(maximumLength: MaxStringLength)]
+        public string ShippingCity { get; set; }
+        [Required]
+        [StringLength(maximumLength: MaxStringLength)]
+        public string ShippingAddress { get; set; }
+    }
+}
---
+++ BpRobotics.Core/Model/CustomerDTOs/ReadCustomerDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BpRobotics.Core.Model.CustomerDTOs
+{
+    public class ReadCustomerDTO
+    {
+        private const int MinZipValue = 1000;
+        private const int MaxZipValue = 9999;
+        private const int MaxStringLength = 100;
+        
+        public int Id { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+        public int VatNumber { get; set; }
+        public int BillingZIP { get; set; }
+        public string BillingCountry { get; set; }
+        public string BillingCity { get; set; }
+        public string BillingAddress { get; set; }
+        public int ShippingZIP { get; set; }
+        public string ShippingCountry { get; set; }
+        public string ShippingCity { get; set; }
+        public string ShippingAddress { get; set; }
+    }
+}
