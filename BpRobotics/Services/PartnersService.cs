ac2aff096d452b19a11f71b2aa8f9dce58fd7111
vmarcell96 <vajdam96@gmail.com>
2022-06-21 13:18:20 +0200

    PartnerService class implemented

---
+++ BpRobotics/Services/PartnersService.cs
@@ -0,0 +1,33 @@
+﻿using BpRobotics.Core.Model;
+using BpRobotics.Data.Entity;
+using BpRobotics.Data.Repositories;
+using BpRobotics.Core.Extensions;
+
+namespace BpRobotics.Services
+{
+    public class PartnersService
+    {
+        private readonly IRepository<Partner> _partnerRepository;
+
+        public PartnersService(IRepository<Partner> partnerRepository)
+        {
+            _partnerRepository = partnerRepository;
+        }
+
+        public async Task<List<Partner>> ListPartners() => await _partnerRepository.GetAll();
+
+        public async Task<Partner> NewPartner(PartnerCreateDto newPartnerDto)
+        {
+            return await _partnerRepository.Add(newPartnerDto.ToPartnerEntity());
+        }
+
+        public async Task<Partner> GetById(int userId) => await _partnerRepository.Get(userId);
+
+        public async Task DeleteById(int userId) => await _partnerRepository.Delete(userId);
+
+        public async Task<Partner> UpdateUser(PartnerUpdateDto updatedUserDto)
+        {
+            return await _partnerRepository.Update(updatedUserDto.ToPartnerEntity());
+        }
+    }
+}
