743dd5163dfb74f8c67de3fdad72390f1f7c02ff
vmarcell96 <vajdam96@gmail.com>
2022-06-21 13:19:35 +0200

    outdated partner model deleted

--- BpRobotics.Core/Model/PartnerModel.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace BpRobotics.Core.Model
-{
-    public class PartnerModel
-    {
-        public string CompanyName { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
