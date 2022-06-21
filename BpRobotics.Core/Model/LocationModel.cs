c89d2fa5c5ff5e489efd153d46cdf182dcd1a30c
Balázs Begov <bbegov@googlemail.com>
2022-06-21 13:15:40 +0200

    Location model created

---
+++ BpRobotics.Core/Model/LocationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BpRobotics.Core.Model
+{
+    public class LocationModel
+    {
+        public int ZIP { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString() => $"{ZIP}, {Country}, {City}, {Address}";
+    }
+}
