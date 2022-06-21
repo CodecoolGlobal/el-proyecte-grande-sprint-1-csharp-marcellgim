3f44007516c524dd2580bb91417524e6a8aa58cf
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-06-21 13:44:29 +0200

    Create Product entity extension methods

---
+++ BpRobotics.Core/Extensions/ProductExtensions.cs
@@ -0,0 +1,67 @@
+﻿using BpRobotics.Core.Model.Product;
+using BpRobotics.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Core.Extensions
+{
+    public static class ProductExtensions
+    {
+        public static Product ToProductEntity(this ProductViewDto productView)
+        {
+            return new Product
+            {
+                ID = productView.Id,
+                Name = productView.Name,
+                ImageFileName = productView.ImageFileName,
+                ServiceInterval = productView.ServiceInterval,
+                Warranty = productView.Warranty,
+                ShortDescription = productView.ShortDescription,
+                LongDescription = productView.LongDescription
+            };
+        }
+
+        public static Product ToProductEntity(this ProductCreateDto productView)
+        {
+            return new Product
+            {
+                Name = productView.Name,
+                ImageFileName = productView.ImageFileName,
+                ServiceInterval = productView.ServiceInterval,
+                Warranty = productView.Warranty,
+                ShortDescription = productView.ShortDescription,
+                LongDescription = productView.LongDescription
+            };
+        }
+
+        public static ProductViewDto ToProductViewDto(this Product productEntity)
+        {
+            return new ProductViewDto
+            {
+                Id = productEntity.ID,
+                Name = productEntity.Name,
+                ImageFileName = productEntity.ImageFileName,
+                Warranty = productEntity.Warranty,
+                ServiceInterval = productEntity.ServiceInterval,
+                ShortDescription = productEntity.ShortDescription,
+                LongDescription = productEntity.LongDescription
+            };
+        }
+
+        public static ProductCreateDto ToProductCreateDto(this Product productEntity)
+        {
+            return new ProductCreateDto
+            {
+                Name = productEntity.Name,
+                ImageFileName = productEntity.ImageFileName,
+                Warranty = productEntity.Warranty,
+                ServiceInterval = productEntity.ServiceInterval,
+                ShortDescription = productEntity.ShortDescription,
+                LongDescription = productEntity.LongDescription
+            };
+        }
+    }
+}
