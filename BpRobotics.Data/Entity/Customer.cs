9dbb61a6cea5206b173a640d380c7a0357d2a6ec
Balázs Begov <bbegov@googlemail.com>
2022-06-21 15:18:57 +0200

    Customer properties made nullable

--- BpRobotics.Data/Entity/Customer.cs
+++ BpRobotics.Data/Entity/Customer.cs
@@ -3,8 +3,8 @@
 public class Customer
 {
     public int Id { get; set; }
-    public string CompanyName { get; set; } = String.Empty;
+    public string? CompanyName { get; set; }
     public int VatNumber { get; set; }
-    public Location BillingAddress { get; set; }
-    public Location ShippingAddress { get; set; }
+    public Location? BillingAddress { get; set; }
+    public Location? ShippingAddress { get; set; }
 }
