966f7681ffad5ad1e9ffe306534fc1216d4dd604
vmarcell96 <vajdam96@gmail.com>
2022-06-21 13:19:06 +0200

    Partner DTOs implemented

---
+++ BpRobotics.Core/Model/PartnerModels/PartnerUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BpRobotics.Core.Model
+{
+    public class PartnerUpdateDto
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ BpRobotics.Core/Model/PartnerModels/PartnerViewDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BpRobotics.Core.Model
+{
+    public class PartnerViewDto
+    {
+        public int Id { get; set; } 
+        public string CompanyName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
