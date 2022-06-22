de5f5654269dbd8c50ac38e91c5f43fe3fe408a6
skris92 <szurcsik92@gmail.com>
2022-06-22 15:25:35 +0200

    DeviceDTOs.

---
+++ BpRobotics.Core/Model/Devices/DeviceCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using BpRobotics.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Core.Model.Devices
+{
+    public class DeviceCreateDTO
+    {
+        [Required]
+        public string? Serial { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+    }
+}
---
+++ BpRobotics.Core/Model/Devices/DeviceViewDTO.cs
@@ -0,0 +1,23 @@
+﻿using BpRobotics.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Core.Model.Devices
+{
+    public class DeviceViewDTO
+    {
+        public string? Serial { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string LastMaintenance { get; set; }
+        public string NextMaintenance { get; set; }
+        public string WarrantyUntil { get; set; }
+        public DeviceStatus Status { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerCompanyName { get; set; }
+        public int OrderId { get; set; }
+    }
+}
