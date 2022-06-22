f9455ae8ef41c26854315c2d927310c6ba71e5c7
marcellgim <marcell.gimesi@gmail.com>
2022-06-22 11:00:02 +0200

    Add data seeder class with user dummy data

---
+++ BpRobotics.Data/SeedData.cs
@@ -0,0 +1,72 @@
+﻿using BpRobotics.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BpRobotics.Data;
+
+public class SeedData
+{
+    private readonly BpRoboticsContext _context;
+
+    public SeedData(BpRoboticsContext context)
+    {
+        _context = context;
+    }
+
+    public void Seed()
+    {
+        _context.Database.EnsureCreated();
+
+        if (!_context.Users.Any())
+        {
+            _context.Users.AddRange(
+                new()
+                {
+                    FirstName = "Péter",
+                    LastName = "Király",
+                    UserName = "MainAdmin",
+                    HashedPassword = "1234",
+                    Role = UserRole.Admin
+                },
+                new()
+                {
+                    FirstName = "Sutő",
+                    LastName = "Károly",
+                    UserName = "BossMan",
+                    HashedPassword = "1234",
+                    Role = UserRole.Admin
+                },
+                new()
+                {
+                    FirstName = "István",
+                    LastName = "Takács",
+                    UserName = "RepairMan",
+                    HashedPassword = "1234",
+                    Role = UserRole.Partner,
+                },
+                new()
+                {
+                    FirstName = "Anna",
+                    LastName = "Partner",
+                    UserName = "CreativeUsername",
+                    HashedPassword = "1234",
+                    Role = UserRole.Partner,
+                },
+                new()
+                {
+                    FirstName = "Lajos",
+                    LastName = "Customer",
+                    UserName = "EcseriFourSeasons",
+                    HashedPassword = "1234",
+                    Role = UserRole.Customer,
+                },
+                new()
+                {
+                    FirstName = "Huba",
+                    LastName = "Hűtő",
+                    UserName = "ILoveRefrigerators",
+                    HashedPassword = "1234",
+                    Role = UserRole.Customer,
+                });
+        }
+    }
+}
