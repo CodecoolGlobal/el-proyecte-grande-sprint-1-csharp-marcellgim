c721be02e0502b4648e492ddf98314138a1ec6b0
skris92 <szurcsik92@gmail.com>
2022-06-22 15:26:17 +0200

    DeviceRepository with CRUD.

---
+++ BpRobotics.Data/Repositories/DeviceRepository.cs
@@ -0,0 +1,48 @@
+﻿using BpRobotics.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Data.Repositories
+{
+    public class DeviceRepository : IRepository<Device>
+    {
+        private readonly BpRoboticsContext _context;
+        public DeviceRepository(BpRoboticsContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Device device)
+        {
+            await _context.Devices.AddAsync(device);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var device = await Get(id);
+            _context.Devices.Remove(device);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Device> Get(int id)
+        {
+            return await _context.Devices.FirstAsync(order => order.Id == id);
+        }
+
+        public async Task<List<Device>> GetAll()
+        {
+            return await _context.Devices.ToListAsync();
+        }
+
+        public async Task<Device> Update(Device device)
+        {
+            _context.Devices.Update(device);
+            await _context.SaveChangesAsync();
+            return device;
+        }
+    }
+}
