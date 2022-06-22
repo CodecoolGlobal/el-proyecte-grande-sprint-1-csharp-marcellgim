3048f4965af8c968816177983651562a21ee3905
skris92 <szurcsik92@gmail.com>
2022-06-22 10:21:50 +0200

    Location struct now a class, added id property, added customer navigation property.

--- BpRobotics.Data/Entity/Location.cs
+++ BpRobotics.Data/Entity/Location.cs
@@ -1,9 +1,11 @@
 ﻿namespace BpRobotics.Data.Entity;
 
-public struct Location
+public class Location
 {
+    public int Id { get; set; }
     public int ZIP { get; set; }
     public string Country { get; set; }
     public string City { get; set; }
     public string Address { get; set; }
+    public virtual Customer? Customer { get; set; }
 }
