f9e383ef503dfaddbe7b8aa2b7be55d5d7394791
skris92 <szurcsik92@gmail.com>
2022-06-22 10:19:44 +0200

    Empty extensions file.

---
+++ BpRobotics.Core/Extensions/OrderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Core.Extensions
+{
+    public static class OrderExtensions
+    {
+
+    }
+}
