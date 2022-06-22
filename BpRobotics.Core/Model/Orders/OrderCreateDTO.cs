ea1b28cf17a3e9c1fb54f827c9c91ce4539705a7
skris92 <szurcsik92@gmail.com>
2022-06-22 10:08:26 +0200

    OrderDTOs

---
+++ BpRobotics.Core/Model/Orders/OrderCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using BpRobotics.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Core.Model.Orders
+{
+    public class OrderCreateDTO
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        public int LocationId { get; set; }
+        [Required]
+        public List<int> DeviceIds { get; set; }
+    }
+}
---
+++ BpRobotics.Core/Model/Orders/OrderViewDTO.cs
@@ -0,0 +1,17 @@
+﻿using BpRobotics.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Core.Model.Orders
+{
+    public class OrderViewDTO
+    {
+        public DateTime Date { get; set; }
+        public Customer Customer { get; set; }
+        public Location Address { get; set; }
+        public List<Device> Devices { get; set; }
+    }
+}
