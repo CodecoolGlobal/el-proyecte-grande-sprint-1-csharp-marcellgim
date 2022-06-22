4a577c012f0ec554ef72bfdad24d079222044d17
marcellgim <marcell.gimesi@gmail.com>
2022-06-22 11:41:57 +0200

    Add dummy data to seeder
    Up to the point of compatibility with current app state

--- BpRobotics.Data/SeedData.cs
+++ BpRobotics.Data/SeedData.cs
@@ -66,7 +66,89 @@ public class SeedData
                     UserName = "ILoveRefrigerators",
                     HashedPassword = "1234",
                     Role = UserRole.Customer,
-                });
+                }
+            );
+
+            _context.SaveChanges();
+
+            _context.Partners.AddRange(
+                new()
+                {
+                    CompanyName = "WeFixIt",
+                    PhoneNumber = "+36 69 420 1337"
+                },
+                new()
+                {
+                    CompanyName = "InstallersInc",
+                    PhoneNumber = "+36 44 999 999"
+                }
+            );
+
+            _context.SaveChanges();
+
+            _context.Customers.AddRange(
+                new()
+                {
+                    CompanyName = "Four Seasons",
+                    BillingAddress = new Location
+                    {
+                        City = "Budapest",
+                        Address = "Ecseri út 14.",
+                        Country = "Hungary",
+                        ZIP = 1097
+                    },
+                    ShippingAddress = new Location
+                    {
+                        City = "Budapest",
+                        Address = "Ecseri út 14.",
+                        Country = "Hungary",
+                        ZIP = 1097
+                    },
+                    VatNumber = 123456,
+                },
+                new()
+                {
+                    CompanyName = "Rat Co.",
+                    BillingAddress = new Location
+                    {
+                        City = "Kiskunhalas",
+                        Address = "Áchim András utca 1.",
+                        Country = "Hungary",
+                        ZIP = 6400
+                    },
+                    ShippingAddress = new Location
+                    {
+                        City = "Kiskunhalas",
+                        Address = "Áchim András utca 1.",
+                        Country = "Hungary",
+                        ZIP = 6400
+                    },
+                    VatNumber = 654321,
+                }
+            );
+
+            _context.SaveChanges();
+
+            _context.Products.AddRange(
+                new()
+                {
+                    Name = "Smart Air",
+                    ServiceInterval = 180,
+                    Warranty = 730,
+                    ShortDescription = "Smart air-conditioner",
+                    LongDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                },
+                new()
+                {
+                    Name = "Smart Fridge",
+                    ServiceInterval = 365,
+                    Warranty = 1095,
+                    ShortDescription = "Smart fridge",
+                    LongDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
+            );
+
+            _context.SaveChanges();
         }
     }
 }
