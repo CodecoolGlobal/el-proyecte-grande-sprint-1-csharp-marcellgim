bb8b8a7ad5083a7b46f3cd43496e9f3fddc2a56a
skris92 <szurcsik92@gmail.com>
2022-06-23 14:59:44 +0200

    Related repositories added with CRUD except update.

--- BpRobotics/Services/OrderService.cs
+++ BpRobotics/Services/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using BpRobotics.Core.Model.Orders;
+﻿using BpRobotics.Core.Extensions;
+using BpRobotics.Core.Model.Orders;
 using BpRobotics.Data.Entity;
 using BpRobotics.Data.Repositories;
 
@@ -6,34 +7,56 @@ namespace BpRobotics.Services
 {
     public class OrderService : IOrderService
     {
-        private readonly IRepository<Order> _repository;
+        private readonly IRepository<Order> _orderRepository;
+        private readonly IRepository<Device> _deviceRepository;
+        private readonly IRepository<Customer> _customerRepository;
+        private readonly IRepository<Product> _productRepository;
 
-        public OrderService(IRepository<Order> repository)
+        public OrderService(IRepository<Order> orderRepository, IRepository<Device> deviceRepository, IRepository<Customer> customerRepository, IRepository<Product> productRepository)
         {
-            _repository = repository;
+            _orderRepository = orderRepository;
+            _deviceRepository = deviceRepository;
+            _customerRepository = customerRepository;
+            _productRepository = productRepository;
         }
 
-        public Task Add(OrderCreateDTO order)
+        public async Task<OrderViewDTO> Add(OrderCreateDTO order)
         {
-            throw new NotImplementedException();
+            var orderEntity = order.ToOrderEntity();
+
+            var costumer = _customerRepository.Get(order.CustomerId);
+            foreach (var productIDandQuant in order.ProductIdsAndQuantity)
+            {
+                var product = await _productRepository.Get(productIDandQuant.Key);
+                for (int i = 0; i < productIDandQuant.Value; i++)
+                {
+                    var device = new Device { Product = product, Status = DeviceStatus.InstallPending };
+                    await _deviceRepository.Add(device);
+                    orderEntity.Devices.Add(device);
+                }
+            }
+
+            await _orderRepository.Add(orderEntity);
+
+            return orderEntity.ToOrderView();
         }
 
         public async Task Delete(int id)
         {
-            throw new NotImplementedException();
+            await _orderRepository.Delete(id);
         }
 
-        public Task<OrderViewDTO> Get(int id)
+        public async Task<OrderViewDTO> Get(int id)
         {
-            throw new NotImplementedException();
+            return (await _orderRepository.Get(id)).ToOrderView();
         }
 
-        public Task<List<OrderViewDTO>> GetAll()
+        public async Task<List<OrderViewDTO>> GetAll()
         {
-            throw new NotImplementedException();
+            return (await _orderRepository.GetAll()).Select(o => o.ToOrderView()).ToList();
         }
 
-        public Task<OrderViewDTO> Update(OrderUpdateDTO order)
+        public async Task<OrderViewDTO> Update(OrderUpdateDTO order)
         {
             throw new NotImplementedException();
         }
