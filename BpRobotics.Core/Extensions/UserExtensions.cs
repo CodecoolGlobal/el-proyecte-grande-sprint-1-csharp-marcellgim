dca76be0cbf089833c3936c027c0d637aeb02bbf
marcellgim <marcell.gimesi@gmail.com>
2022-06-23 11:50:32 +0200

    Change namespace names

--- BpRobotics.Core/Extensions/UserExtensions.cs
+++ BpRobotics.Core/Extensions/UserExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-using BpRobotics.Core.Model.User;
+using BpRobotics.Core.Model.UserDTOs;
 using BpRobotics.Data.Entity;
 
 namespace BpRobotics.Core.Extensions
