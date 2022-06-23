95b0d7c1fbebbbc6f5afd79320f1685112aae0d7
skris92 <szurcsik92@gmail.com>
2022-06-23 15:03:38 +0200

    CreateDTO to Entity and Entity To ViewDTO.

--- BpRobotics.Core/Extensions/OrderExtensions.cs
+++ BpRobotics.Core/Extensions/OrderExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BpRobotics.Core.Model.Orders;
+using BpRobotics.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,33 @@ namespace BpRobotics.Core.Extensions
 {
     public static class OrderExtensions
     {
+        public static Order ToOrderEntity(this OrderCreateDTO orderCreateDTO)
+        {
+            return new Order
+            {
+                Date = DateTime.Now,
+            };
+        }
 
+        public static OrderViewDTO ToOrderView(this Order order)
+        {
+            var devices = new Dictionary<string, int>();
+
+            foreach (var device in order.Devices)
+            {
+                int count;
+                devices.TryGetValue(device.Product.Name, out count);
+                devices[device.Product.Name] = count + 1;
+            }
+
+            return new OrderViewDTO
+            {
+                Date = order.Date,
+                CustomerId = order.Customer.Id,
+                CustomerCompanyName = order.Customer.CompanyName,
+                Address = order.Customer.ShippingAddress.ToString(),
+                Devices = devices
+            };
+        }
     }
 }
