9f3716921e7e83971b17612ac9efba84c7d2ee44
skris92 <szurcsik92@gmail.com>
2022-06-23 15:02:18 +0200

    Updated extension methods.

--- BpRobotics.Core/Extensions/DeviceExtensions.cs
+++ BpRobotics.Core/Extensions/DeviceExtensions.cs
@@ -1,10 +1,5 @@
 ﻿using BpRobotics.Core.Model.Devices;
 using BpRobotics.Data.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BpRobotics.Core.Extensions
 {
@@ -20,23 +15,7 @@ namespace BpRobotics.Core.Extensions
                 NextMaintenance = device.NextMaintenance.ToString(),
                 WarrantyUntil = device.WarrantyUntil.ToString(),
                 Status = device.Status,
-                CustomerCompanyName = device.Customer.CompanyName,
                 OrderId = device.Order.Id
-            };
-        }
-
-        public static Device ToDeviceEntity(this DeviceViewDTO deviceView)
-        {
-            return new Device
-            {
-                Serial = deviceView.Serial,
-                ProductId = deviceView.ProductId,
-                LastMaintenance = DateTime.Parse(deviceView.LastMaintenance),
-                NextMaintenance = DateTime.Parse(deviceView.NextMaintenance),
-                WarrantyUntil = DateTime.Parse(deviceView.WarrantyUntil),
-                Status = deviceView.Status,
-                CustomerId = deviceView.CustomerId,
-                OrderId = deviceView.OrderId
             };
         }
 
@@ -45,23 +24,17 @@ namespace BpRobotics.Core.Extensions
             return new Device
             {
                 Serial = deviceCreate.Serial,
-                ProductId = deviceCreate.ProductId,
                 Status = DeviceStatus.InstallPending,
-                CustomerId = deviceCreate.CustomerId,
-                OrderId = deviceCreate.OrderId
             };
         }
 
         public static Device ToDeviceEntity(this DeviceUpdateDTO deviceUpdate, Device deviceToUpdate)
         {
             deviceToUpdate.Serial = deviceUpdate.Serial ?? deviceToUpdate.Serial;
-            deviceToUpdate.ProductId = deviceUpdate.ProductId ?? deviceToUpdate.ProductId;
             deviceToUpdate.LastMaintenance = (deviceUpdate.LastMaintenance != null) ? DateTime.Parse(deviceUpdate.LastMaintenance) : deviceToUpdate.LastMaintenance;
             deviceToUpdate.NextMaintenance = (deviceUpdate.NextMaintenance != null) ? DateTime.Parse(deviceUpdate.NextMaintenance) : deviceToUpdate.NextMaintenance;
             deviceToUpdate.WarrantyUntil = (deviceUpdate.WarrantyUntil != null) ? DateTime.Parse(deviceUpdate.WarrantyUntil) : deviceToUpdate.WarrantyUntil;
             deviceToUpdate.Status = (deviceUpdate.StatusNumber != null) ? (DeviceStatus)deviceUpdate.StatusNumber : deviceToUpdate.Status;
-            deviceToUpdate.CustomerId = deviceUpdate.CustomerId ?? deviceToUpdate.CustomerId;
-            deviceToUpdate.OrderId = deviceUpdate.OrderId ?? deviceToUpdate.OrderId;
 
             return deviceToUpdate;
         }
