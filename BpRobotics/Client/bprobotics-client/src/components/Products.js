da1775d4e15a739be5926e41ae65ac0d39af973e
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-06-24 00:00:19 +0200

    Creating product add frontend - needs refactoring

--- BpRobotics/Client/bprobotics-client/src/components/Products.js
+++ BpRobotics/Client/bprobotics-client/src/components/Products.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react';
 import MaterialTable from 'material-table';
 import { useNavigate } from "react-router-dom";
+import ProductModal from './ProductModal';
+import Button from '@mui/material/Button';
+
 function Products() {
     let url = `${process.env.REACT_APP_HOST_URL}/api/products`;
     const [productData, setProductData] = useState([]);
+    const [show, setShow] = useState(false);
+    const handleOpen = () => setShow(true);
     let navigate = useNavigate(); 
   function routeChange(productId){ 
     let path = productId.toString(); 
@@ -14,20 +19,22 @@ function Products() {
         const data = await res.json();
         return data;
       }
-    
+      const getProductData = async (url) => {
+        const productDataFromApi = await fetchProductData(url);
+        setProductData(productDataFromApi);
+      }
       useEffect(() => {
-        const getProductData = async (url) => {
-          const productDataFromApi = await fetchProductData(url);
-          setProductData(productDataFromApi);
-        }
         getProductData(url);
       }, [url])
-    return ( <MaterialTable
+    return ( <>
+    <Button variant="primary" onClick={handleOpen}>New</Button>
+    <ProductModal show={show} setShow={setShow} onCreate={getProductData}></ProductModal>
+    <MaterialTable
         title="Products"
         columns={[
             { title: '',
             field: 'img',
-            render: item => <img onClick={() => routeChange(item.id)} src={`${process.env.REACT_APP_HOST_URL}/StaticFiles/images/${item.img}`} alt="" width="100" />,
+            render: item => <img onClick={() => routeChange(item.id)} src={`https://bproboticsimages.blob.core.windows.net/images/${item.imageFileName}`} alt="" width="100" />,
             filtering: false
         },
           { title: 'Name', field: 'name' },
@@ -37,7 +44,7 @@ function Products() {
         options={{
           filtering: true
         }}
-      /> );
+      /></> );
 }
 
 export default Products;
