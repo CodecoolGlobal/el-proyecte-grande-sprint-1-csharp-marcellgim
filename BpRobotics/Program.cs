e711c1b67b7a32ed07e546f366ee76146d784163
marcellgim <marcell.gimesi@gmail.com>
2022-06-24 06:53:02 +0200

    Configure avoiding JSON cycles

--- BpRobotics/Program.cs
+++ BpRobotics/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using BpRobotics.Data;
 using BpRobotics.Data.Repositories;
 using BpRobotics.Data.Entity;
@@ -25,7 +26,8 @@ builder.Services.AddCors(options =>
         });
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
