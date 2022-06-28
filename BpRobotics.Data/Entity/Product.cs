09aa1fa124c248630e74d32db8125b7ca89b6e10
marcellgim <marcell.gimesi@gmail.com>
2022-06-28 11:35:56 +0200

    Remove attribute to fix error

--- BpRobotics.Data/Entity/Product.cs
+++ BpRobotics.Data/Entity/Product.cs
@@ -4,10 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class Product
 {
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int ID { get; set; }
     public string Name { get; set; }
-
     public string? ImageFileName { get; set; }
     public int ServiceInterval { get; set; }
     public int Warranty { get; set; }
