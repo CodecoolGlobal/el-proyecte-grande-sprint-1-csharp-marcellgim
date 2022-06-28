8e30857c3598909a305c6480d12b5c07ab438b10
marcellgim <marcell.gimesi@gmail.com>
2022-06-28 09:54:41 +0200

    Add validation to user update DTO

--- BpRobotics.Core/Model/UserDTOs/UserUpdateDto.cs
+++ BpRobotics.Core/Model/UserDTOs/UserUpdateDto.cs
@@ -7,9 +7,12 @@ public class UserUpdateDto
     [Required]
     public int Id { get; set; }
     [Required]
+    [StringLength(32, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 32 characters long")]
     public string FirstName { get; set; }
     [Required]
+    [StringLength(32, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 32 characters long")]
     public string LastName { get; set; }
     [Required]
+    [StringLength(32, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 32 characters long")]
     public string Password { get; set; }
 }
