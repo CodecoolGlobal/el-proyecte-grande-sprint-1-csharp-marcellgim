1bd7eb572783b364afa8c874008674bfb826d738
marcellgim <marcell.gimesi@gmail.com>
2022-07-03 17:40:55 +0200

    Create Service DTOs

---
+++ BpRobotics.Core/Model/ServiceDTOs/ServiceCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿using BpRobotics.Data.Entity;
+
+namespace BpRobotics.Core.Model.ServiceDTOs;
+
+public class ServiceCreateDTO
+{
+    public int? PartnerId;
+    public string Type { get; set; }
+}
---
+++ BpRobotics.Core/Model/ServiceDTOs/ServiceViewDTO.cs
@@ -0,0 +1,14 @@
+﻿using BpRobotics.Data.Entity;
+
+namespace BpRobotics.Core.Model.ServiceDTOs;
+
+public class ServiceViewDTO
+{
+    public int Id { get; set; }
+    public Device Device { get; set; }
+    public Partner? AssignedFor;
+    public DateTime RequestedDate { get; set; }
+    public DateTime DoneDate { get; set; }
+    public string Type { get; set; }
+    public string Status { get; set; }
+}
