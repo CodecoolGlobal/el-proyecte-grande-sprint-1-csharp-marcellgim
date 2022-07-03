4b2c3b9eeada0858acb3cd2519f8b9922bfc94e6
marcellgim <marcell.gimesi@gmail.com>
2022-07-03 17:41:10 +0200

    Create Service extensions

---
+++ BpRobotics.Core/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using BpRobotics.Core.Model.ServiceDTOs;
+using BpRobotics.Data.Entity;
+
+namespace BpRobotics.Core.Extensions;
+
+public static class ServiceExtensions
+{
+    public static Service ToServiceEntity(this ServiceCreateDTO serviceCreateData)
+    {
+        var serviceType = Enum.Parse<ServiceType>(serviceCreateData.Type);
+        return new Service
+        {
+            RequestedDate = DateTime.Now,
+            Status = ServiceStatus.Planned,
+            Type = serviceType
+        };
+    }
+
+    public static ServiceViewDTO ToServiceView(this Service serviceEntity)
+    {
+        return new ServiceViewDTO
+        {
+            Id = serviceEntity.Id,
+            Device = serviceEntity.Device,
+            AssignedFor = serviceEntity.AssignedFor,
+            RequestedDate = serviceEntity.RequestedDate,
+            DoneDate = serviceEntity.DoneDate,
+            Status = serviceEntity.Status.ToString(),
+            Type = serviceEntity.Type.ToString()
+        };
+    }
+}
