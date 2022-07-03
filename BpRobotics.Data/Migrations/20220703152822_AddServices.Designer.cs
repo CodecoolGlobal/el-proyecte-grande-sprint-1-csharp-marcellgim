467af23b85c5ffddedb5deab7f1a3c6e6f8efc87
marcellgim <marcell.gimesi@gmail.com>
2022-07-03 17:45:35 +0200

    Add migration

--- BpRobotics.Data/Migrations/20220703152822_AddServices.Designer.cs
+++ BpRobotics.Data/Migrations/20220703152822_AddServices.Designer.cs
@@ -4,6 +4,7 @@ using BpRobotics.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BpRobotics.Data.Migrations
 {
     [DbContext(typeof(BpRoboticsContext))]
-    partial class BpRoboticsContextModelSnapshot : ModelSnapshot
+    [Migration("20220703152822_AddServices")]
+    partial class AddServices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BpRobotics.Data/Migrations/20220703152822_AddServices.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BpRobotics.Data.Migrations
+{
+    public partial class AddServices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Device_Ticket_TicketId",
+                table: "Device");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_Device_DeviceId",
+                table: "Service");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_Ticket_RelatedTicketId",
+                table: "Service");
+
+            migrationBuilder.DropTable(
+                name: "TicketComment");
+
+            migrationBuilder.DropTable(
+                name: "Ticket");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Service_RelatedTicketId",
+                table: "Service");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Device_TicketId",
+                table: "Device");
+
+            migrationBuilder.DropColumn(
+                name: "RelatedTicketId",
+                table: "Service");
+
+            migrationBuilder.DropColumn(
+                name: "TicketId",
+                table: "Device");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeviceId",
+                table: "Service",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_Device_DeviceId",
+                table: "Service",
+                column: "DeviceId",
+                principalTable: "Device",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_Device_DeviceId",
+                table: "Service");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeviceId",
+                table: "Service",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RelatedTicketId",
+                table: "Service",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicketId",
+                table: "Device",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AssignedForId = table.Column<int>(type: "int", nullable: true),
+                    CreatedById = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ticket_User_AssignedForId",
+                        column: x => x.AssignedForId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Ticket_User_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketComment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedById = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TicketId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketComment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TicketComment_Ticket_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Ticket",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_TicketComment_User_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Service_RelatedTicketId",
+                table: "Service",
+                column: "RelatedTicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Device_TicketId",
+                table: "Device",
+                column: "TicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_AssignedForId",
+                table: "Ticket",
+                column: "AssignedForId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_CreatedById",
+                table: "Ticket",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketComment_CreatedById",
+                table: "TicketComment",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketComment_TicketId",
+                table: "TicketComment",
+                column: "TicketId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Device_Ticket_TicketId",
+                table: "Device",
+                column: "TicketId",
+                principalTable: "Ticket",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_Device_DeviceId",
+                table: "Service",
+                column: "DeviceId",
+                principalTable: "Device",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_Ticket_RelatedTicketId",
+                table: "Service",
+                column: "RelatedTicketId",
+                principalTable: "Ticket",
+                principalColumn: "Id");
+        }
+    }
+}
