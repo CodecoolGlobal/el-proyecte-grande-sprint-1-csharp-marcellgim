a4a506b3f8908ae0b8fd828dcd456ba3447b95a7
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-07-19 15:00:18 +0200

    Change material table to super-responsive-table

--- BpRobotics/Client/bprobotics-client/src/components/Products.js
+++ BpRobotics/Client/bprobotics-client/src/components/Products.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
-import MaterialTable from 'material-table';
 import { useNavigate } from "react-router-dom";
 import ProductModal from './ProductModal';
 import Button from '@mui/material/Button';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 
 function Products() {
     let url = `${process.env.REACT_APP_HOST_URL}/api/products`;
@@ -24,25 +25,32 @@ function Products() {
   useEffect(() => {
     loadProducts();
   }, [])
+
+  const renderProduct = (product) => {
+    return (
+      <Tr>
+        <Td><img onClick={() => routeChange(product.id)} src={`https://bproboticsimages.blob.core.windows.net/images/${product.imageFileName}`} alt="" width="100" /></Td>
+        <Td>{product.name}</Td>
+        <Td>{product.shortDescription}</Td>
+      </Tr>
+    )
+  }
     return ( <>
     <Button variant="primary" onClick={handleOpen}>New</Button>
     <ProductModal show={show} setShow={setShow} onCreate={loadProducts}></ProductModal>
-    <MaterialTable
-        title="Products"
-        columns={[
-            { title: '',
-            field: 'img',
-            render: item => <img onClick={() => routeChange(item.id)} src={`https://bproboticsimages.blob.core.windows.net/images/${item.imageFileName}`} alt="" width="100" />,
-            filtering: false
-        },
-          { title: 'Name', field: 'name' },
-          { title: 'Description', field: 'shortDescription' },
-        ]}
-        data={productData}        
-        options={{
-          filtering: true
-        }}
-      /></> );
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Image</Th>
+          <Th>Name</Th>
+          <Th>Short Description</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {productData.map(renderProduct)}
+      </Tbody>
+    </Table>
+      </> );
 }
 
 export default Products;
