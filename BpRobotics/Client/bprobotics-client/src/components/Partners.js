228568aedb5409ffc752f2d05f488dfa8af3d232
vmarcell96 <vajdam96@gmail.com>
2022-07-20 11:08:48 +0200

    outsourced create and update forms to distinct components from partners.js

--- BpRobotics/Client/bprobotics-client/src/components/Partners.js
+++ BpRobotics/Client/bprobotics-client/src/components/Partners.js
@@ -3,21 +3,21 @@ import MaterialTable from 'material-table';
 import '../App.css';
 import axiosInstance from "../fetch/axiosInstance";
 import useAxiosFetchGet from "../hooks/useAxiosFetchGet";
+import Table from 'react-bootstrap/Table';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+import Button from 'react-bootstrap/Button';
+import LoadingSpin from "react-loading-spin";
+import AddPartner from "./AddPartner";
+import { useNavigate, useParams } from "react-router-dom";
+
 
 
 function Partners() {
 
+	let navigate = useNavigate();
+
 	const [partnerData, setPartnerData] = useState([]);
-
-	const [postCompanyName, setPostCompanyName] = useState('');
-	const [postPhoneNumber, setPostPhoneNumber] = useState('');
-
-	const [editCompanyName, setEditCompanyName] = useState('');
-	const [editPhoneNumber, setEditPhoneNumber] = useState('');
-
-	const [idToDelete, setIdToDelete] = useState('');
-
-	const [idToUpdate, setIdToUpdate] = useState('');
 
 	const url = `${process.env.REACT_APP_HOST_URL}/api/partners`;
 
@@ -25,9 +25,7 @@ function Partners() {
 
 	//cosmetic
 	const [isPendingDelete, setIsPendingDelete] = useState(false);
-	const [isPendingAdd, setIsPendingAdd] = useState(false);
-	const [isPendingUpdate, setIsPendingUpdate] = useState(false);
-	const [selectedRow, setSelectedRow] = useState();
+
 
 	//set data state
 	useEffect(() => {
@@ -35,125 +33,89 @@ function Partners() {
 	}, [data])
 
 
-	const handleSubmit = async (e) => {
-		//e.preventDefault();
-		const newPartner = { "CompanyName" : postCompanyName, "PhoneNumber" : postPhoneNumber };
-		setIsPendingAdd(true);
-		try {
-			const response = await axiosInstance.post(url, newPartner)
-			//response should send back the created object
-			const allPartners = [...partnerData, response.data]
-			setPartnerData(allPartners);
-			setPostCompanyName('');
-			setPostPhoneNumber('');
-		} catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-		setIsPendingAdd(false);
-	}
-
-
-	const handleUpdate = async (e) => {
-		//e.preventDefault();
-		const updatedPartner = { "CompanyName" : editCompanyName, "PhoneNumber" : editPhoneNumber };
-		setIsPendingUpdate(true);
-		try {
-			const response = await axiosInstance.put(`${url}/${idToUpdate}`, updatedPartner)
-			setPartnerData(partnerData.map(partner => partner.id === idToUpdate ? { ...response.data } : partner));
-			setEditCompanyName('');
-			setEditPhoneNumber('');
-			setIdToUpdate('');
-		} catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-		setIsPendingUpdate(false);
-	}
-
-
-	const handleDelete = async (e) => {
-		//e.preventDefault();
+	const handleDelete = async (id) => {
 		setIsPendingDelete(true);
 		try {
-			await axiosInstance.delete(`${url}/${idToDelete}`);
-			const allPartners = partnerData.filter(partner => partner.id !== idToDelete);
+			await axiosInstance.delete(`${url}/${id}`);
+			const allPartners = partnerData.filter(partner => partner.id !== id);
 			setPartnerData(allPartners);
-			setIdToDelete('');
 		} catch (err) {
 			console.log(`Error: ${err.message}`);
 		}
-		setIsPendingDelete(true);
+		setTimeout(() => { setIsPendingDelete(false) }, 1500);
 	}
 
 
 
 	return (
 		<>
-			<div className="create">
-				<h2>Add new Partner</h2>
-				<form onSubmit={handleSubmit}>
-					<label>Company name:</label>
-					<input 
-						type="text" 
-						required 
-						value={postCompanyName}
-						onChange={(e) => setPostCompanyName(e.target.value)}
-					/>
-					<label>Phone number:</label>
-					<input 
-						type="text" 
-						required
-						value={postPhoneNumber}
-						onChange={(e) => setPostPhoneNumber(e.target.value)} />
-					{ !isPendingAdd && <button>Add partner</button> }
-					{ isPendingAdd && <button>Adding partner...</button> }
-				</form>
-			</div>
-			<div className="create">
-				<h2>Update Partner</h2>
-				<form onSubmit={handleUpdate}>
-					<label>Id:</label>
-					<input 
-						type="text" 
-						required 
-						value={idToUpdate}
-						onChange={(e) => setIdToUpdate(e.target.value)}
-					/>
-					<label>Company name:</label>
-					<input 
-						type="text" 
-						required 
-						value={editCompanyName}
-						onChange={(e) => setEditCompanyName(e.target.value)}
-					/>
-					<label>Phone number:</label>
-					<input 
-						type="text" 
-						required
-						value={editPhoneNumber}
-						onChange={(e) => setEditPhoneNumber(e.target.value)} />
-					{ !isPendingUpdate && <button>Update partner</button> }
-					{ isPendingUpdate && <button>Updating partner...</button> }
-				</form>
-			</div>
-			<div className="create">
-				<h2>Delete Partner</h2>
-				<form onSubmit={handleDelete}>
-					<label>Id:</label>
-					<input 
-						type="text" 
-						required 
-						value={idToDelete}
-						onChange={(e) => setIdToDelete(e.target.value)}
-					/>
-					{ !isPendingDelete && <button>Delete partner</button> }
-					{ isPendingDelete && <button>Deleting partner...</button> }
-				</form>
-			</div>
 			<div style={{ maxWidth: '100%' }}>
-				{isLoading && <h1>LOADING</h1>}
-				{fetchError && <p style={{color: "red"}}>{fetchError}</p>}
-				{!isLoading && !fetchError && 
-				<MaterialTable
+				{isLoading && <h1><LoadingSpin /></h1>}
+				{fetchError && <p style={{ color: "red" }}>{fetchError}</p>}
+				{!isLoading && !fetchError && partnerData &&
+					<>
+						<Table striped="columns">
+							<thead>
+								<tr>
+									<th>
+										<td>
+											<>
+												<Button onClick={() => { navigate("/partners/add") }}>
+													<FontAwesomeIcon icon={faPlus} />
+												</Button>
+											</>
+										</td>
+									</th>
+									<th>Company Name</th>
+									<th>Phone Number</th>
+								</tr>
+							</thead>
+							<tbody>
+								{partnerData.map((partner) =>
+									<tr key={partner.id}>
+										<td>#</td>
+										<td>{partner.companyName}</td>
+										<td>{partner.phoneNumber}</td>
+
+										<td>
+											{!isPendingDelete ?
+												<>
+													<Button onClick={() => { navigate(`/partners/${partner.id}`) }}>
+														<FontAwesomeIcon icon={faEdit} />
+													</Button>
+													<Button onClick={() => { handleDelete(partner.id) }}>
+														<FontAwesomeIcon icon={faTrash} />
+													</Button>
+												</>
+												:
+												<><LoadingSpin
+													width="6px"
+													primaryColor="yellow"
+													size="30px"
+												/></>}
+										</td>
+									</tr>
+								)}
+							</tbody>
+						</Table>
+					</>
+				}
+			</div>
+		</>
+	);
+}
+
+export default Partners;
+
+
+
+
+
+
+
+
+
+{/* <MaterialTable
 				onRowClick={(evt, selectedRow) =>
 					setSelectedRow(selectedRow.tableData.id)
 				}
@@ -201,11 +163,4 @@ function Partners() {
 				]}
 				data={partnerData}
 				title="Partners"
-				/>
-				}
-			</div>
-		</>
-	);
-}
-
-export default Partners;
+				/> */}
