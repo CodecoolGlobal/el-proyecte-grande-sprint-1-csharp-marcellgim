ccb42c90be71b6a061d4319c155b2cf16b59bafa
marcellgim <marcell.gimesi@gmail.com>
2022-07-20 20:38:12 +0200

    Create device list page

---
+++ BpRobotics/Client/bprobotics-client/src/components/Devices.js
@@ -0,0 +1,43 @@
+import { Table, Alert } from "react-bootstrap";
+import LoadingSpin from "react-loading-spin";
+import useAxiosFetchGet from "../hooks/useAxiosFetchGet";
+
+const DEVICES_ENDPOINT = "/api/devices";
+
+function Devices() {
+    const { data: devices, isLoading, fetchError } = useAxiosFetchGet(DEVICES_ENDPOINT);
+    const render = (input) => input; // Default render
+    const dateRender = (date) => new Date(date).toLocaleDateString();
+
+    const columns = [
+        { title: "Serial Number", field: "serial", render: (serial) => serial ?? "-"},
+        { title: "Product", field: "productName", render },
+        { title: "Last Maintenance", field: "lastMaintenance", render: dateRender},
+        { title: "Next Maintenance", field: "nextMaintenance", render: dateRender},
+        { title: "Warranty Expires", field: "warrantyUntil", render: dateRender},
+        { title: "Device Status", field: "status", render}
+    ]
+
+    return (
+        <Table striped="columns">
+            <thead>
+                <tr>
+                    {columns.map((column, index) => <th key={index}>{column.title}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {isLoading && <tr><td><LoadingSpin /></td></tr>}
+				{fetchError && <Alert variant='danger'>{fetchError}</Alert>}
+                {devices.map(device => (
+                    <tr key={device.id}>
+                        {columns.map((column, index) => (
+							<td key={index}>{column.render(device[column.field])}</td>
+						))}
+                    </tr>
+				))}
+            </tbody>
+        </Table>
+    );
+}
+
+export default Devices;
