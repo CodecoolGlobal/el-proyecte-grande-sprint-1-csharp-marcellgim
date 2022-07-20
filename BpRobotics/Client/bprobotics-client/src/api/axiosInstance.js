8892625ddc0d9bc39ddbd4a547769505a92a7680
marcellgim <marcell.gimesi@gmail.com>
2022-07-20 11:20:04 +0200

    Remove redundant axios instance

--- BpRobotics/Client/bprobotics-client/src/api/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = `${process.env.REACT_APP_HOST_URL}/api`
-
-export default axios.create({
-    baseURL: BASE_URL,
-    headers: {'Content-Type': 'application/json' }
-})
