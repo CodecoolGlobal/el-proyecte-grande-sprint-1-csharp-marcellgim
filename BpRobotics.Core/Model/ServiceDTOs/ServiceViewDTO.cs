d1a4a85a16d3418362e5d57d38c01697f71b2f0b
marcellgim <marcell.gimesi@gmail.com>
2022-07-20 21:47:05 +0200

    Make Service done-date nullable (includes migration)

--- BpRobotics.Core/Model/ServiceDTOs/ServiceViewDTO.cs
+++ BpRobotics.Core/Model/ServiceDTOs/ServiceViewDTO.cs
@@ -7,7 +7,7 @@ public class ServiceViewDTO
     public int Id { get; set; }
     public PartnerViewDto? AssignedFor { get; set; }
     public DateTime RequestedDate { get; set; }
-    public DateTime DoneDate { get; set; }
+    public DateTime? DoneDate { get; set; }
     public string Type { get; set; }
     public string Status { get; set; }
 }
