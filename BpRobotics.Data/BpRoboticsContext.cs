01d7811679d65d420fcf424d7eee785e6d1c4376
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-07-21 16:10:32 +0200

    Delete unnecessary code from context

--- BpRobotics.Data/BpRoboticsContext.cs
+++ BpRobotics.Data/BpRoboticsContext.cs
@@ -30,25 +30,18 @@ namespace BpRobotics.Data
                 .OwnsOne(e => e.ShippingAddress);
 
             modelBuilder.Entity<Product>().ToTable("Product");
-            modelBuilder.Entity<Product>().Property<bool>("IsDeleted");
-            modelBuilder.Entity<Product>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
             modelBuilder.Entity<Order>().ToTable("Orders");
             modelBuilder.Entity<Partner>().ToTable("Partner");
             modelBuilder.Entity<User>().ToTable("User");
             modelBuilder.Entity<Device>().ToTable("Device");
             modelBuilder.Entity<Service>().ToTable("Service");
-            modelBuilder.Entity<User>().Property<bool>("IsDeleted");
-            modelBuilder.Entity<User>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
-            modelBuilder.Entity<Customer>().Property<bool>("IsDeleted");
-            modelBuilder.Entity<Customer>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
-            modelBuilder.Entity<Order>().Property<bool>("IsDeleted");
-            modelBuilder.Entity<Order>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
-            modelBuilder.Entity<Device>().Property<bool>("IsDeleted");
-            modelBuilder.Entity<Device>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
-            modelBuilder.Entity<Service>().Property<bool>("IsDeleted");
-            modelBuilder.Entity<Service>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
-            modelBuilder.Entity<Partner>().Property<bool>("IsDeleted");
-            modelBuilder.Entity<Partner>().HasQueryFilter(m => EF.Property<bool>(m, "IsDeleted") == false);
+            modelBuilder.Entity<Product>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<User>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Customer>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Order>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Device>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Service>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Partner>().HasQueryFilter(m => !m.IsDeleted);
 
 
         }
