084bec10101fab3b9dbeb01d0e86bc0e9147e6da
skris92 <szurcsik92@gmail.com>
2022-08-03 13:42:40 +0200

    Link button for customers/add

--- BpRobotics/Client/bprobotics-client/src/components/Customers.js
+++ BpRobotics/Client/bprobotics-client/src/components/Customers.js
@@ -2,21 +2,36 @@ import { Link } from 'react-router-dom';
 import { Table, Alert } from 'react-bootstrap';
 import LoadingSpin from 'react-loading-spin';
 import useAxiosFetchGet from "../hooks/useAxiosFetchGet";
+import useAuth from '../hooks/useAuth';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 
 const CUSTOMERS_URL = "/api/customers";
 
 function Customers() {
+  const { auth } = useAuth();
   const { data: customers, isLoading, fetchError } = useAxiosFetchGet(CUSTOMERS_URL);
   const render = (input) => input; // Default render
   const columns = [
     { title: 'Company Name', field: 'companyName', render },
     { title: 'VAT number', field: 'vatNumber', render }
   ];
+  let navigate = useNavigate();
 
   return (
     <Table striped="columns">
       <thead>
         <tr>
+          <th>
+            {auth?.role === "Admin" && <>
+              <Button onClick={() => { navigate("/customers/add") }}>
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+            </>}
+          </th>
           {columns.map((column, index) => <th key={index}>{column.title}</th>)}
         </tr>
       </thead>
@@ -25,6 +40,7 @@ function Customers() {
         {fetchError && <tr><td><Alert variant='danger'>{fetchError}</Alert></td></tr>}
         {customers.map(customer => (
           <tr key={customer.id}>
+            <td>#</td>
             <td><Link to={customer.id.toString()} state={customer}>{customer.companyName}</Link></td>
             <td>{customer.vatNumber}</td>
           </tr>
