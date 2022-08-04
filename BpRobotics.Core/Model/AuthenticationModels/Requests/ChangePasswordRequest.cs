45d729d2c814e3eb664b0b96bcfd78ee1df78b58
vmarcell96 <vajdam96@gmail.com>
2022-08-04 15:54:36 +0200

    created changepasswordrequest class

---
+++ BpRobotics.Core/Model/AuthenticationModels/Requests/ChangePasswordRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpRobotics.Core.Model.AuthenticationModels
+{
+    public class ChangePasswordRequest
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string OldPassword { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
