be389bad921ba7322294e3b5c9a93d7adde41a56
marcellgim <marcell.gimesi@gmail.com>
2022-08-04 14:21:16 +0200

    Wrap table in card component

--- BpRobotics/Client/bprobotics-client/src/components/ServicesView.js
+++ BpRobotics/Client/bprobotics-client/src/components/ServicesView.js
@@ -1,4 +1,4 @@
-import { Table } from 'react-bootstrap';
+import { Table, Card } from 'react-bootstrap';
 import useAxiosFetchGet from "../hooks/useAxiosFetchGet";
 import { faPlay, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import useAxios from "../hooks/useAxios";
@@ -12,65 +12,67 @@ import Button from 'react-bootstrap/Button';
 const url = "/api/service";
 
 function ServicesView() {
-	const axiosInstance = useAxios();
+  const axiosInstance = useAxios();
 
   const { data: services, setData: setServices, isLoading, fetchError } = useAxiosFetchGet(url);
   const render = (input) => input; // Default render
   const dateRender = (date) => (date !== null) ? new Date(date).toLocaleDateString() : "-";
 
   const columns = [
-    { title: "Partner", field: "assignedFor", render: (partner) => partner?.companyName ?? "-"},
+    { title: "Partner", field: "assignedFor", render: (partner) => partner?.companyName ?? "-" },
     { title: "Requested on", field: "requestedDate", render: dateRender },
-    { title: "Done on", field: "doneDate", render: dateRender},
-    { title: "Service Type", field: "type", render},
-    { title: "Status", field: "status", render}
+    { title: "Done on", field: "doneDate", render: dateRender },
+    { title: "Service Type", field: "type", render },
+    { title: "Status", field: "status", render }
   ];
 
   const changeServiceStatus = async (id, change) => {
-		try {
-			await axiosInstance.put(`${url}/${id}/${change}`);
-      services.filter(service=>service.id===id)[0].status = change === "start" ? "InProgress" : "Done";
-      if(change === "finish") {
-        services.filter(service=>service.id===id)[0].doneDate = new Date();
+    try {
+      await axiosInstance.put(`${url}/${id}/${change}`);
+      services.filter(service => service.id === id)[0].status = change === "start" ? "InProgress" : "Done";
+      if (change === "finish") {
+        services.filter(service => service.id === id)[0].doneDate = new Date();
       }
       setServices([...services]);
-		} 
+    }
     catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-	}
+      console.log(`Error: ${err.message}`);
+    }
+  }
 
   return (
-    <Table>
-            <thead>
-                <tr>
-                    {columns.map((column, index) => <th key={index}>{column.title}</th>)}
-                    <th>Start</th>
-                    <th>Finish</th>
-                </tr>
-            </thead>
-            <tbody>
-                {services.map(service => (
-                    <>
-                        <tr key={service.id}>
-                            {columns.map((column, index) => (
-                                <td key={index}>{column.render(service[column.field])}</td>
-                            ))}
-                            <td>
-                            <Button disabled={service.status!=="Planned"} onClick={() => { changeServiceStatus(service.id, "start") }}>
-														<FontAwesomeIcon icon={faPlay} />
-													</Button>
-                          </td>
-                          <td>
-													<Button disabled={service.status!=="InProgress"} onClick={() => { changeServiceStatus(service.id, "finish") }}>
-														<FontAwesomeIcon icon={faCircleCheck} />
-													</Button>
-                          </td>
-                        </tr>
-                    </>
-				))}
-            </tbody>
-        </Table>
+    <Card body>
+      <Table>
+        <thead>
+          <tr>
+            {columns.map((column, index) => <th key={index}>{column.title}</th>)}
+            <th>Start</th>
+            <th>Finish</th>
+          </tr>
+        </thead>
+        <tbody>
+          {services.map(service => (
+            <>
+              <tr key={service.id}>
+                {columns.map((column, index) => (
+                  <td key={index}>{column.render(service[column.field])}</td>
+                ))}
+                <td>
+                  <Button disabled={service.status !== "Planned"} onClick={() => { changeServiceStatus(service.id, "start") }}>
+                    <FontAwesomeIcon icon={faPlay} />
+                  </Button>
+                </td>
+                <td>
+                  <Button disabled={service.status !== "InProgress"} onClick={() => { changeServiceStatus(service.id, "finish") }}>
+                    <FontAwesomeIcon icon={faCircleCheck} />
+                  </Button>
+                </td>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </Table>
+    </Card>
   );
 }
 
